7f689053692d6fc56a2091747694b49061c72fae
Piwero <piwero@gmail.com>
2024-05-17 08:24:24 +0100

    fix(upload-chartmuseum): Convert releaserc file to js to pass env variable

---
+++ .releaserc.js
@@ -0,0 +1,25 @@
+
+module.exports = {
+    "branches": [
+        "main"
+    ],
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                "changelogFile": "charts/helm-1/CHANGELOG.md"
+            }
+        ],
+        [
+            'semantic-release-helm3',
+            {
+                chartPath: 'charts/helm-1',
+                onlyUpdateVersion: true,
+                registry: process.env.CM_REGISTRY,
+                isChartMuseum: true
+            }
+        ]
+    ]
+}

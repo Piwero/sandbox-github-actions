a03179807af339171d6280789350b4c6858ed198
Piwero <piwero@gmail.com>
2024-06-03 06:30:32 +0100

    ci: Set up for multiple repos release

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
     "branches": [
         "main"
     ],
-    "extends": "semantic-release-monorepo",
+    "tagFormat": process.env.CHART_NAME + "-v${version}",
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
@@ -12,11 +12,27 @@ module.exports = {
                 "changelogFile": "CHANGELOG.md"
             }
         ],
+        [
+            "@semantic-release/exec",
+            {
+                "prepareCmd": "helm-docs",
+                "publishCmd": "cr package . && cr upload -o piwero -r sandbox-github-actions -c $(git rev-parse HEAD) --release-notes-file CHANGELOG.md --push --packages-with-index --skip-existing"
+            }
+        ],
+        [
+            "semantic-release-helm3",
+            {
+                "chartPath": ".",
+                "populateChangelog": "true",
+                "onlyUpdateVersion": "true"
+            }
+        ],
+
         "@semantic-release/git",
         {
             "assets": [
-                "Charts/helm-1/Chart.yaml",
-                "Charts/app-2/Chart.yaml",
-            ]
+                "Chart.yaml",
+            ],
+            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }]
 }
